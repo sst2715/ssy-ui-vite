cab940c8c54adeeaee8653b695fe2164a17d0033
sst2715 <leilei_2715@qq.com>
2024-10-25 10:41:01 +0800

    Add GitHub Actions workflow

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -1,6 +1,6 @@
 declare module '*.vue' {
   import type { ComponentOptions } from 'vue'
 
-  const component: ComponentOptions<{}, {}, unknown, object> // 根据需要使用 'object' 或 'unknown'
+  const component: ComponentOptions<{}, {}, object, object> // 根据需要使用 'object' 或 'unknown'
   export default component
 }
