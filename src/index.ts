0a3753f9a75420327ffb218a85a08a13fadd6463
sst2715 <leilei_2715@qq.com>
2024-10-29 10:34:44 +0800

    按需引入

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue/dist/vue.esm-browser'
+import { createApp } from 'vue';
 import SSYUI from './entry'
 
 createApp({
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,46 @@
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-import UnoCSS from 'unocss/vite'
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import type { BuildOptions } from "vite";
+import { resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite";
 
-const rollupOptions = {
-  external: ['vue', 'vue-router'],
+const rollupOptions: BuildOptions["rollupOptions"] = {
+  external: ["vue"], // 将这些模块保留在 bundle 之外
   output: {
     globals: {
-      vue: 'Vue',
+      vue: "Vue",
     },
+    exports: "named",
   },
-}
-
+};
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    UnoCSS(),
-  ],
-  build: {
-    rollupOptions,
-    minify: false,
-    cssCodeSplit: true,
-    lib: {
-      entry: './src/entry.ts',
-      name: 'SSYUI',
-      fileName: 'ssy-ui',
-      formats: ['es', 'umd', 'iife'],
+  plugins: [vue(), vueJsx(), UnoCSS()],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler",
     },
   },
   test: {
-    // 启用类似 jest 的全局测试 API
     globals: true,
-    // 使用 happy-dom 模拟 DOM
-    // （需要将 happy-dom 作为 peer 依赖安装）
-    environment: 'happy-dom',
+    environment: "happy-dom",
   },
-})
+
+  build: {
+    rollupOptions,
+    minify: "terser", // boolean | 'terser' | 'esbuild'
+    sourcemap: false, // 输出单独 source文件
+    reportCompressedSize: true, // 生成压缩大小报告
+    cssCodeSplit: true,
+    // 添加库模式配置
+    lib: {
+      entry: resolve(__dirname, "src/entry.ts"),
+      name: "SSYUI",
+      fileName: "ssy-ui",
+      // 导出模块格式
+      formats: ["es", "umd"],
+    },
+  },
+});
